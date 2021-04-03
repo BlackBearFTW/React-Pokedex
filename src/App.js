999e3f47b31cbd97860a909814160115f3601baf
BlackBearFTW <robinmager@home.nl>
2021-04-03 09:36:04 +0200

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Autocomplete from "./components/Autocomplete";
+
+
+
 
 function App() {
+   const [pokemon, setPokemon] = useState([]);
+    const [pokemonInformation, setPokemonInformation] = useState("");
+    const [pokemonImg, setPokemonImg] = useState("");
+
+    async function getInformation(pokemon) {
+        if (pokemon === "") {
+            setPokemonImg("");
+            return setPokemonInformation("");
+        }
+
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+        setPokemonInformation(JSON.stringify(res.data, null, "\t"));
+        await getImage(res.data);
+    }
+
+    async function getImage(data) {
+        const res = await axios.get(data.forms[0].url);
+        setPokemonImg(res.data.sprites.front_default);
+    }
+
+
+    useEffect(() => {
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=1118').then(res => {
+           setPokemon(res.data.results.map(poke => poke.name));
+        });
+    }, []);
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Autocomplete options={pokemon} limit="10" callback={getInformation}></Autocomplete>
+        <img src={pokemonImg} alt=""/>
+        <pre>{pokemonInformation}</pre>
     </div>
   );
 }
---
+++ src/components/Autocomplete.js
@@ -0,0 +1,44 @@
+import {useState} from "react";
+
+function Autocomplete({options, limit, callback}) {
+    const [suggestions, setSuggestions] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+
+    function handleChange(event) {
+        setInputValue(event.target.value);
+        callback("");
+
+        if (event.target.value === "") return setSuggestions([]);
+
+        const items = options.filter(op => {
+            return op.toLowerCase().startsWith(event.target.value);
+        })
+
+        setSuggestions(items.slice(0, limit));
+    }
+
+
+
+    function handleClick(event) {
+        setInputValue(event.target.innerText);
+        setSuggestions([]);
+
+        callback(event.target.innerText);
+    }
+
+
+    return (
+        <div>
+            <input type="text" onChange={handleChange} value={inputValue}/>
+            <ul className="suggestions">
+                {suggestions.map(suggestion => (
+                    <li key={suggestion} onClick={handleClick}>
+                        {suggestion}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Autocomplete;
